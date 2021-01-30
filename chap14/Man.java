2904cb7d146a419f5d2d4ba2e7aafeb36f096625
wysss <1243876958@qq.com>
2021-01-31 05:33:18 +0800

    instanceof方法

---
+++ chap14/Man.java
@@ -0,0 +1,3 @@
+public class Man extends Human{
+    
+}
---
+++ chap14/Test.java
@@ -0,0 +1,7 @@
+public class Test {
+    public static void main(String[] args) {
+        Human p2 = new Man();
+        System.out.println(p2 instanceof Man);
+    }
+
+}
